d8d5806e6c0c23ec2447c84e05170a983939b914
kovalmaximv <kovalmaximv@gmail.com>
2023-03-01 23:15:38 +0300

    git config fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TypeIt from "typeit-react";
 function Intro(props) {
   return (
     <div style={{fontSize: "48px"}} className="terminal">
-      <div /*style={{backgroundColor: 'gray'}}*/>
+      <div /* style={{backgroundColor: 'gray'}} */>
         <div style={{background: 'url(/traffic_lights.svg) center center / 100% 100%', height: '1rem', width: '6rem', marginLeft: '0.5rem', marginTop: '0.8rem'}}></div>
       </div>
       <div style={{padding: '0.5rem 1rem'}}>
